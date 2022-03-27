2ff0a89195e68c5fc2cf03a92a88904aa43f0f2a
walid-tab <walid.tabbabi7@gmail.com>
2022-03-27 19:40:41 +0100

    checkpoint_JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import SimpleForm from "./components/SimpleForm";
+import image from "./imageInSrc.jpg";
+import './style.css';
+
 
 
 function App() {
     return (
-      <div>
-        <SimpleForm />
+    <header>
+      <div style={{border:"solid 1px black" , width:"100vw"}}>
+        <h1 className="title red">Your name here</h1>
+            <div className="img">
+            <img className="imgSrc" src={image}/>
+            <img className="imgPub"src="imageInPublic.jpg"/>
+            </div>
+            
       </div>
+              <video width="320" height="240" controls>
+                  <source src="myVideo.mp4" type="video/mp4" ></source>
+              </video>
+    </header>
     );   
 }
 
