530c844253a308a7fcde84f2ddff5f018d1f1794
walid-tab <walid.tabbabi7@gmail.com>
2022-03-27 20:03:38 +0100

    checkpoint_REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import image from "./imageInSrc.jpg";
-import './style.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SimpleForm from "./components/SimpleForm";
 
 
 function App() {
     return (
-    <header>
-      <div style={{border:"solid 1px black" , width:"100vw"}}>
-        <h1 className="title red">Your name here</h1>
-            <div className="img">
-            <img className="imgSrc" src={image}/>
-            <img className="imgPub"src="imageInPublic.jpg"/>
-            </div>
-            
+      <div>
+        <SimpleForm />
       </div>
-              <video width="320" height="240" controls>
-                  <source src="myVideo.mp4" type="video/mp4" ></source>
-              </video>
-    </header>
     );   
 }
 
